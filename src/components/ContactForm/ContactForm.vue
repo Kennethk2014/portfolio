68d44efe5083b4187712b96be9ad353f583de259
Kenneth Kristiansen <kennethk2014@gmail.com>
2022-03-30 17:12:21 +0200

    form update

--- src/components/ContactForm/ContactForm.vue
+++ src/components/ContactForm/ContactForm.vue
@@ -9,20 +9,26 @@
         <input type="email" name="user_email" class="form-control" id="email" placeholder="ada@lovelace.com">
         <!-- About the project  -->
         <label for="projectInfo" class="form-label mt-3">Tell me about your project</label>
-        <textarea name="message" class="form-control" id="projectInfo" rows="3"></textarea>
+        <textarea required name="message" class="form-control" id="projectInfo" rows="3"></textarea>
+        <custom-button type="submit" value="Send" class="btn-primary mt-3"></custom-button>
       </form>
     </div>    
 </template>
 
 <script>
 import emailjs from '@emailjs/browser';
+import CustomButton from '../CustomButton/CustomButton.vue';
 export default {
+  components: { CustomButton },
     methods: {
         sendEmail(){
+
             emailjs.sendForm('contact_service', 'contact_form', this.$refs.form, 'EhNQFb7NUNPCi3mcM')
             .then((result) => {
                 console.log('SUCCESS!', result.text);
+                alert("Message sent, thank you.")
             }, (error) => {
+                alert("Message failed. Sorry we're having problems right now...")
                 console.log('FAILED...', error.text);
             });
         }
