64ca54519ed7c90c675ade4846e9f322c510f011
Kenneth Kristiansen <kennethk2014@gmail.com>
2022-06-05 13:06:16 +0200

    page transition anim

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
 <the-navbar></the-navbar>
   <div class="app">
+    <Transition name="slide-fade">
       <router-view />
+
+    </Transition>
   </div>
 </template>
 
@@ -34,6 +37,19 @@ export default {
 p{
   font-family: 'Montserrat', sans-serif;
 
+}
+.slide-fade-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 
 </style>
