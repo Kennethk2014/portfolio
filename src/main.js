ac6c697a34c554cf197361bb258e830de3870213
Kenneth Kristiansen <kennethk2014@gmail.com>
2022-06-14 13:51:38 +0200

    analytics update

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue';
 import router from './router';
 import '@fortawesome/fontawesome-free/css/all.css';
 import VueKinesis from 'vue-kinesis';
-
+import VueGtag from 'vue-gtag';
 
 
 createApp(App)
 .use(router)
+.use(VueGtag, {
+    config: {id: 'UA-209804149-1'}
+})
 .use(VueKinesis)
 .mount('#app')
 import 'bootstrap/dist/js/bootstrap.js';
